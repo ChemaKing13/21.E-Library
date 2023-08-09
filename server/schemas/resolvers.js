c5c83c9521b5a033a486d1fc58e0e7bd3ab69ec8
ChemaKing13 <jm2000ho@gmail.com>
2023-08-09 12:39:43 -0600

    typeDefs.js updated

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,2 +1,13 @@
-//define the query and mutation functionality to work with
-//the Mongoose models
+//define the query and mutation functionality to work with the Mongoose models
+
+//get a single user
+
+// create a user 
+
+//login 
+
+// saveBook 
+
+//removeBooke 
+
+
